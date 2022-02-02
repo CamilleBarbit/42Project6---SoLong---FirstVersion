13d84553c753faa45187a50a254f5d0712732c08
Camille BARBIT <cbarbit@e2r12p10.clusters.42paris.fr>
2022-02-02 15:00:51 +0100

    new version, with struct

--- main.c
+++ main.c
@@ -2,13 +2,13 @@
 
 int handle_mouse(int keycode, void *param)
 {
-    printf("the keycode is: %d\n", keycode);
+    // printf("the keycode is: %d\n", keycode);
     return (0);
 }
 
 int handle_key(int keycode, void *param)
 {
-    printf("the keycode is: %d\n", keycode);
+    // printf("the keycode is: %d\n", keycode);
     if (keycode == 65307)
         exit(1);
     return (0);
@@ -16,14 +16,23 @@ int handle_key(int keycode, void *param)
 
 int main()
 {
-    void    *mlx;
-    void    *window;
+    map     so_long;
 
-    mlx = mlx_init();
-    if (!mlx)
+    so_long.x = 0;
+    so_long.y = 0;
+    so_long.mlx = mlx_init();
+    if (!so_long.mlx)
         return (0);
-    window = mlx_new_window(mlx, 400, 400, "First Window!");
-    mlx_mouse_hook(window, handle_mouse, 0);
-    mlx_key_hook(window, handle_key, 0);
-    mlx_loop(mlx);
+    so_long.window = mlx_new_window(so_long.mlx, 800, 800, "First Window!");
+    //while (position.x < 400 && position.y < 400)
+    // {
+    //     mlx_pixel_put(mlx, window, position.x, position.y, 0x00CCFFCC);
+    //     position.x++;
+    //     position.y++;
+    // }
+    so_long.image = mlx_new_image(so_long.mlx, 800, 800);
+    mlx_put_image_to_window(so_long.mlx, so_long.window, so_long.image, so_long.x, so_long.y);
+    mlx_mouse_hook(so_long.window, handle_mouse, 0);
+    mlx_key_hook(so_long.window, handle_key, 0);
+    mlx_loop(so_long.mlx);
 }
