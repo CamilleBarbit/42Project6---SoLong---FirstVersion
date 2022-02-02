1a2d3ce34dc6110da5e126422a95c176c56a21bf
Camille BARBIT <cbarbit@e2r12p10.clusters.42paris.fr>
2022-02-02 12:09:52 +0100

    updated main

--- main.c
+++ main.c
@@ -1,4 +1,20 @@
 #include "minilibx/mlx.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int handle_mouse(int keycode, void *param)
+{
+    printf("the keycode is: %d\n", keycode);
+    return (0);
+}
+
+int handle_key(int keycode, void *param)
+{
+    printf("the keycode is: %d\n", keycode);
+    if (keycode == 65307)
+        exit(1);
+    return (0);
+}
 
 int main()
 {
@@ -7,6 +23,7 @@ int main()
 
     mlx = mlx_init();
     window = mlx_new_window(mlx, 400, 400, "First Window!");
+    mlx_mouse_hook(window, handle_mouse, 0);
+    mlx_key_hook(window, handle_key, 0);
     mlx_loop(mlx);
-
 }
