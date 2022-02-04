0f2cd2cfe4deeb939500a5d424858039c57f5229
Camille BARBIT <cbarbit@e2r4p15.clusters.42paris.fr>
2022-02-04 16:00:25 +0100

    update version/it is working!

--- checkerror.c
+++ checkerror.c
@@ -1,36 +1,25 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   checkerror.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: cbarbit <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/04 15:48:21 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/02/04 15:48:30 by cbarbit          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "so_long.h"
 
-void	ft_check_walls(char **tab, char *file_name,int size) //je lui envoie le nombre de lignes
+int	ft_check_walls(char **tab, char *file_name,int size) //je lui envoie le nombre de lignes
 {
 	int	x;
 	int	y;
 
 	if (!tab)
-		exit (1);
+		return (1);
 	x = 0;
 	while (tab[0][x + 1]) //tant que le caractère à (x + 1) != 0 -> il n'ira pas sur le \n car après il y a un \0
 	{
 		if (tab[0][x] != '1')
-			exit(1);
+			return (1);
 		x++;
 	}
 	x = 0;
 	while (tab[size - 1][x + 1])
 	{
 		if (tab[size - 1][x] != '1')
-			exit(1);
+			return (1);
 		x++;
 	}
 	y = 1;
@@ -41,6 +30,7 @@ void	ft_check_walls(char **tab, char *file_name,int size) //je lui envoie le nom
 			exit(1);
 		y++;
 	}
+	return (0);
 }
 
 int	main()
