18c2eb963ccca420fbed7195b958148f06b6ca02
Camille BARBIT <cbarbit@e2r1p10.clusters.42paris.fr>
2022-02-07 13:04:19 +0100

    updted version / slipt check map & file

--- checkerror_map.c
+++ checkerror_map.c
@@ -2,25 +2,6 @@
 #include "so_long.h"
 #include "string.h"
 
-int	ft_strcmp(char *s1, char *s2)
-{
-	int	i;
-
-	i = 0;
-	while (s1[i] && s2[i])
-	{
-		if (s1[i] != s2[i])
-		{
-			printf("je suis different");
-			return (1);
-		}
-		i++;
-	}
-	printf("je suis le même");
-	return (0);
-}
-
-
 int	ft_check_walls(char **tab, char *file_name, int size) //je lui envoie le nombre de lignes
 {
 	int	x;
@@ -73,29 +54,6 @@ int	ft_check_map_form(char **tab, char *file_name, int size) //je lui envoie le 
 	}
 	printf("C'est la même taille");
 	return (0);
-}
-
-int	ft_check_file_name(char *file_name)
-{
-	int	i;
-	char	*str;
-
-	i = 0;
-	str = ".ber";
-	while (file_name[i])
-	{
-		if (file_name[i] == '.')
-		{
-			if (ft_strcmp(file_name + i, str) == 0)
-			{
-				printf("Bon nom");
-				return (0);
-			}
-		}
-		i++;
-	}
-	printf("Mauvais nom");
-	return (1);
 }
 
 // void	ft_handle_all_errors(char **tab, char *file_name, int size)
