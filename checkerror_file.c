6bd7d7aed82df3e70b0ba24e572d7bbfc98c3e06
Camille BARBIT <cbarbit@e2r1p10.clusters.42paris.fr>
2022-02-07 13:03:46 +0100

    first version

---
+++ checkerror_file.c
@@ -0,0 +1,43 @@
+#include "so_long.h"
+#include "string.h"
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+		{
+			printf("je suis different");
+			return (1);
+		}
+		i++;
+	}
+	printf("je suis le même");
+	return (0);
+}
+
+int	ft_check_file_name(char *file_name)
+{
+	int	i;
+	char	*str;
+
+	i = 0;
+	str = ".ber";
+	while (file_name[i])
+	{
+		if (file_name[i] == '.')
+		{
+			if (ft_strcmp(file_name + i, str) == 0)
+			{
+				printf("Bon nom");
+				return (0);
+			}
+		}
+		i++;
+	}
+	printf("Mauvais nom");
+	return (1);
+}
