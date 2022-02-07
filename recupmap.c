73a7b2fad68c96d9d1f085f787b4d581397d93f8
Camille BARBIT <cbarbit@e2r1p10.clusters.42paris.fr>
2022-02-07 17:46:53 +0100

    new version

--- recupmap.c
+++ recupmap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   recupmap.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:36:01 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/03 13:26:09 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/02/07 17:43:39 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,13 +58,54 @@ char	**ft_print_map(char *file_name)
 	while (nb_line > 0)
 	{
 		str_map[i] = get_next_line(fd);
-		printf("%s", str_map[i]);
+		//printf("%s", str_map[i]);
 		i++;
 		nb_line--;
 	}
 	str_map[i] = 0;
 	close(fd);
 	return (str_map);
+}
+
+int	ft_count_char(char *str, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	if (!str)
+		return (1);
+	while (str[i])
+	{
+		if (str[i] == c)
+			count += 1;
+		i++;
+	}
+	return (count);
+}
+
+int	ft_check_map_char(char **tab, int size)
+{
+	int	x;
+	int	y;
+	int	*tab_count[3] = {0, 0, 0};
+
+	x = 0;
+	y = 1;
+	while (y <= size - 2)
+	{
+		tab_count[0] += ft_count_char(tab[y], 'P');
+		tab_count[1] += ft_count_char(tab[y], 'C');
+		tab_count[2] += ft_count_char(tab[y], 'E');
+		y++;
+	}
+	printf("PP %d\n", tab_count[0]);
+	printf("CC %d\n", tab_count[1]);
+	printf("EE %d\n", tab_count[2]);
+	if (tab_count[0] != 1 || tab_count[1] < 1 || tab_count[2] < 1)
+		return (write(1, "Error\nSome characters are missing!\n", 34), 1);
+	return (write(1, "TOTO BEN\n", 9), 0);
 }
 
 // int main()
@@ -98,7 +139,7 @@ int	main()
 
 	size = ft_recup_map_size(file_name);
 	tab = ft_print_map(file_name);
-	check_map_char(tab, size);
+	ft_check_map_char(tab, size);
 	//nb_line = ft_recup_map_size(file_name);
 	//ft_check_walls(tab, file_name, size);
 	return (0);
