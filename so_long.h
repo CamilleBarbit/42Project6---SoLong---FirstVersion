5f37da0e02ce135540869e3760d4015baff7e0e9
Camille BARBIT <cbarbit@e2r2p7.clusters.42paris.fr>
2022-02-08 14:55:04 +0100

    updated version with struct / beginning

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:34:06 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/08 11:39:53 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/08 14:54:46 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct map map;
-struct map
+typedef struct SoLong SoLong;
+struct SoLong
 {
     void        *mlx;
     void        *window;
     void        *image;
 };
 
-// struct  s_bonjour
-// {
-//     char **map;
-// };
+typedef struct Map Map;
+struct  Map
+{
+    char	**map_tab;
+	char	*file_name;
+    int		map_size;
+};
 
-int	ft_check_map_form(char **tab, int size);
-int	ft_check_walls(char **tab, char *file_name, int size);
+int		ft_recup_map_size(char *file_name);
+void	ft_print_map(struct Map *map_ptr);
+int		ft_check_map_form(char **tab, int size);
+int		ft_check_walls(char **tab, char *file_name, int size);
+void	ft_free_map(char **tab, int size);
+char	*sdup(char *src);
 
 #endif
