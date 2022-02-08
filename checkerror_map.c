050a3130a121288e6c0d806835f7550c0910d494
Camille BARBIT <cbarbit@e2r2p7.clusters.42paris.fr>
2022-02-08 10:41:06 +0100

    added ft_free_map to avoid leaks

--- checkerror_map.c
+++ checkerror_map.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 15:44:20 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/02/07 19:32:30 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/08 10:38:53 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,14 @@ int	ft_check_map_walls(char **tab, char *file_name, int size) //je lui envoie le
 	while (tab[0][x + 1]) //tant que le caractère à (x + 1) != 0 -> il n'ira pas sur le \n car après il y a un \0
 	{
 		if (tab[0][x] != '1')
-			return (write(1, "Error\nWalls are missing!\n", 25), 1);
+			return (ft_free_map(tab, size), write(1, "Error\nWalls are missing!\n", 25), 1);
 		x++;
 	}
 	x = 0;
 	while (tab[size - 1][x + 1])
 	{
 		if (tab[size - 1][x] != '1')
-			return (write(1, "Error\nWalls are missing!\n", 25), 1);
+			return (ft_free_map(tab, size), write(1, "Error\nWalls are missing!\n", 25), 1);
 		x++;
 	}
 	y = 1;
@@ -50,30 +50,10 @@ int	ft_check_map_walls(char **tab, char *file_name, int size) //je lui envoie le
 	{
 		x = strlen(tab[y]); //strlen est la taille de la string en question - on ne veut pas la taille du tableau
 		if(tab[y][0] != '1' || tab[y][x - 2] != '1') //je mets x - 2 car avant le \0, il y a un \n
-			return (write(1, "Error\nWalls are missing!\n", 25), 1);
+			return (ft_free_map(tab, size), write(1, "Error\nWalls are missing!\n", 25), 1);
 		y++;
 	}
-	return (0);
-}
-
-int	ft_check_map_form(char **tab, int size)
-{
-	int		tempo;
-	int		len;
-	int		y;
-	int		x;
-
-	tempo = strlen(tab[0]);
-	y = 1;
-	x = 0;
-	while (y <= size - 1)
-	{
-		len = strlen(tab[y]);
-		if (len != tempo)
-			return (write(1, "Error\nString lengths are not the same!\n", 33), 1);
-		y++;
-	}
-	return (0);
+	return (ft_free_map(tab, size), write(1, "Tout va bien\n", 12), 0);
 }
 
 static int	ft_count_char(char *str, char c)
@@ -110,10 +90,36 @@ int	ft_check_map_char(char **tab, int size)
 		y++;
 	}
 	if (tab_count[0] != 1 || tab_count[1] < 1 || tab_count[2] < 1)
+	{
+		ft_free_map(tab, size);
 		return (write(1, "Error\nSome characters are missing!\n", 34), 1);
+	}
+	ft_free_map(tab, size);	
 	return (0);
 }
 
+
+int	ft_check_map_form(char **tab, int size) //NO LEAKS!
+{
+	int		tempo;
+	int		len;
+	int		y;
+
+	tempo = strlen(tab[0]);
+	y = 1;
+	while (y <= size - 1)
+	{
+		len = strlen(tab[y]);
+		if (len != tempo)
+		{
+			ft_free_map(tab, size);
+			return (write(1, "Error\nString lengths are not the same!\n", 33), 1);
+		}
+		y++;
+	}
+	ft_free_map(tab, size);
+	return (0);
+}
 
 // void	ft_handle_all_errors(char **tab, char *file_name, int size)
 // {
