7bf3ff7da06bb3e72e13e06249d4539dc750405b
Camille BARBIT <cbarbit@e2r2p7.clusters.42paris.fr>
2022-02-08 15:03:31 +0100

    We are discovering structures

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:34:16 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/08 14:51:12 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/08 15:02:45 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,84 +14,6 @@
 
 /* Je fais une fonction qui remplit les variables
 que j'ai dans ma structure */
-
-int ft_len(char *str)
-{
-    int i;
-
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
-}
-
-int	ft_recup_map_size(char *file_name)
-{
-	char	*line;
-	int		total_nb_line;
-	int		fd;
-
-	total_nb_line = 0;
-	fd = open(file_name, O_RDONLY);
-	line = get_next_line(fd);
-	while (line)
-	{
-		total_nb_line++;
-		free(line);
-		line = get_next_line(fd);
-	}
-	free(line);
-	close (fd);
-	if (total_nb_line < 3)
-		return (write(1, "Error\nProblem with the number of lines\n", 36), -1);
-	return (total_nb_line);
-}
-
-void	ft_print_map(struct Map *map_ptr)
-{
-	int		fd;
-	int		i;
-	int		nb_line;
-
-	i = 0;
-	nb_line = map_ptr->map_size;
-	if (nb_line == -1)
-		return ;
-	fd = open(map_ptr->file_name, O_RDONLY);
-	map_ptr->map_tab[i] = malloc(sizeof(char *) * (nb_line + 1));
-	if (!map_ptr->map_tab)
-		return ;
-	while (nb_line > 0)
-	{
-		map_ptr->map_tab[i] = get_next_line(fd);
-		printf("%s", map_ptr->map_tab[i]);
-		i++;
-		nb_line--;
-	}
-	map_ptr->map_tab[i] = 0;
-	close(fd);
-}
-
-char	*sdup(char *src)
-{
-	char	*dest;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = ft_len(src);
-	dest = malloc(sizeof(char) * j + 1);
-	if (!dest)
-		return (0);
-	while (i < j)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
 
 
 void	ft_filler(Map *map_ptr, char *argv1)
