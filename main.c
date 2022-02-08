c2d848a66315c5f762bee36bc2b88a283e96e1d6
Camille BARBIT <cbarbit@e2r2p7.clusters.42paris.fr>
2022-02-08 14:59:18 +0100

    We are discovering structures

--- main.c
+++ main.c
@@ -6,11 +6,101 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:34:16 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/08 11:36:48 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/08 14:51:12 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+/* Je fais une fonction qui remplit les variables
+que j'ai dans ma structure */
+
+int ft_len(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+int	ft_recup_map_size(char *file_name)
+{
+	char	*line;
+	int		total_nb_line;
+	int		fd;
+
+	total_nb_line = 0;
+	fd = open(file_name, O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
+	{
+		total_nb_line++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	free(line);
+	close (fd);
+	if (total_nb_line < 3)
+		return (write(1, "Error\nProblem with the number of lines\n", 36), -1);
+	return (total_nb_line);
+}
+
+void	ft_print_map(struct Map *map_ptr)
+{
+	int		fd;
+	int		i;
+	int		nb_line;
+
+	i = 0;
+	nb_line = map_ptr->map_size;
+	if (nb_line == -1)
+		return ;
+	fd = open(map_ptr->file_name, O_RDONLY);
+	map_ptr->map_tab[i] = malloc(sizeof(char *) * (nb_line + 1));
+	if (!map_ptr->map_tab)
+		return ;
+	while (nb_line > 0)
+	{
+		map_ptr->map_tab[i] = get_next_line(fd);
+		printf("%s", map_ptr->map_tab[i]);
+		i++;
+		nb_line--;
+	}
+	map_ptr->map_tab[i] = 0;
+	close(fd);
+}
+
+char	*sdup(char *src)
+{
+	char	*dest;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = ft_len(src);
+	dest = malloc(sizeof(char) * j + 1);
+	if (!dest)
+		return (0);
+	while (i < j)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+
+
+void	ft_filler(Map *map_ptr, char *argv1)
+{
+	map_ptr->file_name = ft_sdup(argv1);
+	map_ptr->map_size = ft_recup_map_size(map_ptr->file_name);
+	map_ptr->map_tab = ft_print_map(map_ptr);
+} 
+
 
 // int handle_mouse(int keycode, void *param)
 // {
@@ -51,12 +141,24 @@
 
 int main(int argc, char **argv)
 {
-    struct s_bonjour *bonjour;
+    struct Map	*map_ptr = NULL;
+
     if (argc == 2)
     {
-        if (ft_check_file_name(argv[1]) == 1)
-            return (1);
-        if (ft_print_map(argv[1]))
+		map_ptr = malloc(sizeof(struct Map));
+		if (!map_ptr)
+			return (1);
+		ft_filler(map_ptr, argv[1]); //je lui envoie l'adresse de ma struct 
+
+
+
+
+
+
+        // if (ft_check_file_name(argv[1]) == 1)
+        //     return (1);
+        // if (ft_print_map(argv[1]))
         
     }
+	return (0);
 }
