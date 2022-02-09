a409ff6afbae6c73f696a6092093318e5ef0c8e6
Camille Barbit <barbitcamille@gmail.com>
2022-02-09 11:37:11 +0100

    added functions to so_long.h

--- so_long.h
+++ so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:34:06 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/08 14:54:46 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/09 11:24:44 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@
 typedef struct SoLong SoLong;
 struct SoLong
 {
-    void        *mlx;
-    void        *window;
-    void        *image;
+    void	*mlx;
+    void	*window;
+    void	*image;
 };
 
-typedef struct Map Map;
-struct  Map
+typedef struct s_Map s_Map;
+struct  s_Map
 {
     char	**map_tab;
 	char	*file_name;
@@ -35,10 +35,13 @@ struct  Map
 };
 
 int		ft_recup_map_size(char *file_name);
-void	ft_print_map(struct Map *map_ptr);
+char	**ft_print_map(struct s_Map *map_ptr);
 int		ft_check_map_form(char **tab, int size);
 int		ft_check_walls(char **tab, char *file_name, int size);
 void	ft_free_map(char **tab, int size);
-char	*sdup(char *src);
+char	*ft_sdup(char *src);
+int		ft_len(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_check_file_name(char *file_name);
 
 #endif
