54a1b065e643da1657d3b9e6aaa6a32036a67718
Camille Barbit <barbitcamille@gmail.com>
2022-02-09 12:08:43 +0100

    Updated version / implementation of struct variables

--- checkerror_map.c
+++ checkerror_map.c
@@ -3,44 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   checkerror_map.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 15:44:20 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/02/08 14:09:58 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/09 12:03:53 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include "string.h"
 
-int	ft_check_map_walls(char **tab, char *file_name, int size) //je lui envoie le nombre de lignes
+int	ft_check_map_walls(struct s_Map *map_ptr)
 {
 	int	x;
 	int	y;
 
 	x = 0;
-	while (tab[0][x + 1])
+	while (map_ptr->map_tab[0][x + 1])
 	{
-		if (tab[0][x] != '1')
-			return (ft_free_map(tab, size), write(1, "Error\nWalls are missing!\n", 25), 1);
+		if (map_ptr->map_tab[0][x] != '1')
+			return (write(1, "Error\nWalls are missing!\n", 25), 1);
 		x++;
 	}
 	x = 0;
-	while (tab[size - 1][x + 1])
+	while (map_ptr->map_tab[size - 1][x + 1])
 	{
-		if (tab[size - 1][x] != '1')
-			return (ft_free_map(tab, size), write(1, "Error\nWalls are missing!\n", 25), 1);
+		if (map_ptr->map_tab[size - 1][x] != '1')
+			return (write(1, "Error\nWalls are missing!\n", 25), 1);
 		x++;
 	}
 	y = 1;
-	while (y <= size - 2)
+	while (y <= map_ptr->map_size - 2)
 	{
-		x = strlen(tab[y]); 
-		if(tab[y][0] != '1' || tab[y][x - 2] != '1')
-			return (ft_free_map(tab, size), write(1, "Error\nWalls are missing!\n", 25), 1);
+		x = ft_len(map_ptr->map_tab[y]); 
+		if(map_ptr->map_tab[y][0] != '1' || map_ptr->map_tab[y][x - 2] != '1')
+			return (write(1, "Error\nWalls are missing!\n", 25), 1);
 		y++;
 	}
-	return (ft_free_map(tab, size), write(1, "Tout va bien\n", 12), 0);
+	return (write(1, "Tout va bien\n", 12), 0);
 }
 
 static int	ft_count_char(char *str, char c)
@@ -61,49 +61,51 @@ static int	ft_count_char(char *str, char c)
 	return (count);
 }
 
-int	ft_check_map_char(char **tab, int size)
+int	ft_check_map_char(struct s_Map *map_ptr)
 {
-	int	x;
 	int	y;
 	int	tab_count[3] = {0, 0, 0};
 
-	x = 0;
 	y = 1;
-	while (y <= size - 2)
+	while (y <= map_ptr->map_size - 2)
 	{
-		tab_count[0] += ft_count_char(tab[y], 'P');
-		tab_count[1] += ft_count_char(tab[y], 'C');
-		tab_count[2] += ft_count_char(tab[y], 'E');
+		tab_count[0] += ft_count_char(map_ptr->map_tab[y], 'P');
+		tab_count[1] += ft_count_char(map_ptr->map_tab[y], 'C');
+		tab_count[2] += ft_count_char(map_ptr->map_tab[y], 'E');
 		y++;
 	}
 	if (tab_count[0] != 1 || tab_count[1] < 1 || tab_count[2] < 1)
-	{
-		ft_free_map(tab, size);
 		return (write(1, "Error\nSome characters are missing!\n", 34), 1);
-	}
-	ft_free_map(tab, size);	
 	return (0);
 }
 
 
-int	ft_check_map_form(char **tab, int size)
+int	ft_check_map_form(struct s_Map *map_ptr)
 {
 	int		tempo;
 	int		len;
 	int		y;
 
-	tempo = strlen(tab[0]);
+	tempo = ft_len(map_ptr->map_tab[0]);
 	y = 1;
-	while (y <= size - 1)
+	while (y <= map_ptr->map_size - 1)
 	{
-		len = strlen(tab[y]);
+		len = ft_len(map_ptr->map_tab[y]);
 		if (len != tempo)
-		{
-			ft_free_map(tab, size);
 			return (write(1, "Error\nString lengths are not the same!\n", 33), 1);
 		}
 		y++;
 	}
-	ft_free_map(tab, size);
+	return (0);
+}
+
+int	ft_check_all_errors(struct s_Map *map_ptr)
+{
+	if (ft_check_map_form(map_ptr) == 1)
+		return (ft_free_map(map_ptr), 1);
+	if (ft_check_map_walls(map_ptr) == 1)
+		return (ft_free_map(map_ptr), 1);
+	if (ft_check_map_char(map_ptr) == 1)
+		return (ft_free_map(map_ptr), 1);
 	return (0);
 }
