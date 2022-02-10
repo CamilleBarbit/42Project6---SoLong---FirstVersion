182a57b3e19f7c2e293118471f0700c085701f28
Camille Barbit <barbitcamille@gmail.com>
2022-02-10 16:31:28 +0100

    updated version with new file

---
+++ errors/checkerror_file.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checkerror_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/07 15:44:11 by cbarbit           #+#    #+#             */
+/*   Updated: 2022/02/09 16:06:15 by camillebarb      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+#include "string.h"
+
+int	ft_check_file_name(char *file_name)
+{
+	int		i;
+	char	*str;
+    int		count;
+
+	i = 0;
+	str = ".ber";
+    count = 0;
+	while (file_name[i])
+	{
+		if (file_name[i] == '.' && count == 0)
+		{
+            count = 1;
+			if (ft_strcmp(file_name + i, str) == 0  && file_name[i + 4] == '\0' && ft_len(file_name) > 4)
+				return (0);
+		}
+		i++;
+	}
+	return(printf("Error\nProblem with file name\n"), 1);
+}
