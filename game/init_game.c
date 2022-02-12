903a7223945e1db3f4ef98dab6c72c3797598ad5
Camille Barbit <barbitcamille@gmail.com>
2022-02-12 12:41:59 +0100

    new version

--- game/init_game.c
+++ game/init_game.c
@@ -6,8 +6,15 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 11:47:07 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/12 11:47:17 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/02/12 12:41:10 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void    init_game(struct s_Map *map_ptr)
+{
+    map_ptr->mlx_ptr = mlx_init();
+    map_ptr->mlx_new_window = (map_ptr->mlx_ptr, X, Y, map_ptr->map_title);
+
+}
