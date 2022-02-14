1a5ebc0ea65db0341be3acc59d99d06f8403f220
Camille Barbit <barbitcamille@gmail.com>
2022-02-14 11:48:42 +0100

    Updated version - parameters modified

--- game/print_map.c
+++ game/print_map.c
@@ -6,13 +6,13 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 11:32:48 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/12 13:27:45 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/02/14 11:41:20 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../so_long.h"
 
-void ft_draw_map(struct s_Map *map_ptr)
+void	ft_draw_map(struct s_Map *map_ptr)
 {
 	int		x;
 	int		y;
@@ -24,15 +24,15 @@ void ft_draw_map(struct s_Map *map_ptr)
 		while (map_ptr->map_tab[y][x])
 		{
 			if (map_ptr->map_tab[y][x] == '1')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_walls, 32 * x, 32 * y);
+				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_walls, 32 * map_ptr->size_x, 32 * map_ptr->map_size);
 			else if (map_ptr->map_tab[y][x] == 'C')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_items, 32 * x, 32 * y);
+				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_items, 32 * map_ptr->size_x, 32 * map_ptr->map_size);
 			else if (map_ptr->map_tab[y][x] == 'P')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_player, 32 * x, 32 * y);
+				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_player, 32 * map_ptr->size_x, 32 * map_ptr->map_size);
 			else if (map_ptr->map_tab[y][x] == 'E')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_exit, 32 * x, 32 * y);
+				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_exit, 32 * map_ptr->size_x, 32 * map_ptr->map_size);
 			else if (map_ptr->map_tab[y][x] == '0')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_empty_spaces, 32 * x, 32 * y);
+				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_empty_spaces, 32 * map_ptr->size_x, 32 * map_ptr->map_size);
 			x++;
 		}
 		y++;
