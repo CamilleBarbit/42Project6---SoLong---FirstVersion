cfed23f112f2f8d7d41dcdcdb7e3694571cf4fb1
Camille Barbit <barbitcamille@gmail.com>
2022-02-14 11:47:16 +0100

    Added new functions for items and player position

---
+++ game/game_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 11:46:15 by camillebarb       #+#    #+#             */
+/*   Updated: 2022/02/14 11:46:41 by camillebarb      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "../so_long.h"
+
+void	get_player_position(struct s_Map *map_ptr)
+{
+    int x;
+    int y;
+
+    x = 0;
+    y = 0;
+    while (map_ptr->map_tab[y])
+    {
+        x = 0;
+        while (map_ptr->map_tab[y][x])
+        {
+            if (map_ptr->map_tab[y][x] == 'P')
+            {
+                map_ptr->player_position_x = x;
+                map_ptr->player_position_y = y;
+                map_ptr->map_tab[y][x] = '0';
+            }
+            x++;
+        }
+        y++;
+    }
+}
+
+//option1
+void	get_number_of_items(int	*tab_count)
+{
+	map_ptr->count_items = tab_count[1];
+}
+
+//option2
+int	get_number_of_items(struct s_Map *map_ptr)
+{
+	int	x;
+	int	y;
+	int	count;
+
+	x = 0;
+	y = 0;
+	count = 0;
+	while (map_ptr->map_tab[y])
+	{
+		x = 0;
+		while (map_ptr->map_tab[y][x])
+		{
+			if (map_ptr->map_tab[y][x] == 'C')
+				count += 1;
+			x++;
+		}
+		y++;
+	}
+	return (count);
+}
