784fe3927e555c8d42a03ec5f70539c044b88333
Camille Barbit <barbitcamille@gmail.com>
2022-02-14 16:02:09 +0100

    updated version of player's mouv

--- game/player.c
+++ game/player.c
@@ -6,7 +6,7 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 12:24:05 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/14 12:15:20 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/02/14 15:59:24 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 static void    ft_move_up(struct s_Map *map_ptr)
 {
-    if(map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] !=1)
+    if(map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] != 1)
     {
         map_ptr->player_position_y--;
+		get_items(map_ptr);
     }
+	else
+		printf("Error\nMoving forward is impossible!");
 }
 
 static void    ft_move_left(struct s_Map *map_ptr)
@@ -25,7 +28,10 @@ static void    ft_move_left(struct s_Map *map_ptr)
     if(map_ptr->map_tab[map_ptr->player_position_y][map_ptr->player_position_x - 1] !=1)
     {
         map_ptr->player_position_x--;
+		get_items(map_ptr);
     }
+	else
+		printf("Error\nMoving left is impossible!");
 }
 
 static void    ft_move_down(struct s_Map *map_ptr)
@@ -33,7 +39,10 @@ static void    ft_move_down(struct s_Map *map_ptr)
     if(map_ptr->map_tab[map_ptr->player_position_y + 1][map_ptr->player_position_x] !=1)
     {
         map_ptr->player_position_y++;
+		get_items(map_ptr);
     }
+	else
+		printf("Error\nMoving down is impossible!");
 }
 
 static void    ft_move_right(struct s_Map *map_ptr)
@@ -41,7 +50,11 @@ static void    ft_move_right(struct s_Map *map_ptr)
     if(map_ptr->map_tab[map_ptr->player_position_y][map_ptr->player_position_x + 1] !=1)
     {
         map_ptr->player_position_x++;
+		get_items(map_ptr);
     }
+	else
+		printf("Error\nMoving right is impossible!");
+}
 }
 
 int key_hook(int key code, struct s_Map *map_ptr) //This is the function called
