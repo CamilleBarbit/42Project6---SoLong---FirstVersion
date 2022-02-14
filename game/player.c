e01d1585beab43d8f21833afcebe19802dfd568a
Camille Barbit <barbitcamille@gmail.com>
2022-02-14 12:16:07 +0100

    suppressed a function and put it in game_utils.c

--- game/player.c
+++ game/player.c
@@ -6,13 +6,13 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 12:24:05 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/12 19:56:43 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/02/14 12:15:20 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-void    ft_move_up(struct s_Map *map_ptr)
+static void    ft_move_up(struct s_Map *map_ptr)
 {
     if(map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] !=1)
     {
@@ -20,7 +20,7 @@ void    ft_move_up(struct s_Map *map_ptr)
     }
 }
 
-void    ft_move_left(struct s_Map *map_ptr)
+static void    ft_move_left(struct s_Map *map_ptr)
 {
     if(map_ptr->map_tab[map_ptr->player_position_y][map_ptr->player_position_x - 1] !=1)
     {
@@ -28,7 +28,7 @@ void    ft_move_left(struct s_Map *map_ptr)
     }
 }
 
-void    ft_move_down(struct s_Map *map_ptr)
+static void    ft_move_down(struct s_Map *map_ptr)
 {
     if(map_ptr->map_tab[map_ptr->player_position_y + 1][map_ptr->player_position_x] !=1)
     {
@@ -36,35 +36,11 @@ void    ft_move_down(struct s_Map *map_ptr)
     }
 }
 
-void    ft_move_right(struct s_Map *map_ptr)
+static void    ft_move_right(struct s_Map *map_ptr)
 {
     if(map_ptr->map_tab[map_ptr->player_position_y][map_ptr->player_position_x + 1] !=1)
     {
         map_ptr->player_position_x++;
-    }
-}
-
-void    get_player_position(struct s_Map *map_ptr)
-{
-    int x;
-    int y;
-
-    x = 0;
-    y = 0;
-    while (map_ptr->map_tab[y])
-    {
-        x = 0;
-        while (map_ptr->map_tab[y][x])
-        {
-            if (map_ptr->map_tab[y][x] == 'P')
-            {
-                map_ptr->player_position_x = x;
-                map_ptr->player_position_y = y;
-                map_ptr->map_tab[y][x] = '0';
-            }
-            x++;
-        }
-        y++;
     }
 }
 
