33f1bc0500c8d066fffdccecccec65f2ce74e18e
Camille BARBIT <cbarbit@e2r4p11.clusters.42paris.fr>
2022-02-16 16:43:35 +0100

    What changed?

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:34:16 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/16 14:59:53 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/16 15:13:45 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_filler_basics(s_Map *map_ptr, char *str)
 	map_ptr->file_name = ft_sdup(str); //nom de mon ficher .ber
 	map_ptr->map_size = ft_recup_map_size(map_ptr->file_name); //taille de ma map (y)
 	map_ptr->map_tab = ft_recup_map(map_ptr); //Je récupère la map dans un char **
-	map_ptr->size_x = ft_len(map_ptr->map_tab[0]); //La taille de ma première ligne
+	map_ptr->size_x = ft_len(map_ptr->map_tab[0]) - 1; //La taille de ma première ligne - le '\n'
 	ft_strcpy(map_ptr->map_title, "so_long"); //Le nom de ma fenêtre de jeu
 	map_ptr->width = 32;
 	map_ptr->heigth = 32;
