4d9f01f678232f38874d275d9b97f5d6c00583cd
Camille BARBIT <cbarbit@e2r4p23.clusters.42paris.fr>
2022-02-17 11:59:36 +0100

    added functions + defines key codes

--- so_long.h
+++ so_long.h
@@ -6,17 +6,21 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:34:06 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/16 13:35:54 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/17 11:51:04 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+/* LIBRARIES */
+
 #include "mlx.h"
 #include "get_next_line/get_next_line.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+/* STRUCTURE */
 
 typedef struct s_Map s_Map;
 struct  s_Map
@@ -33,6 +37,7 @@ struct  s_Map
 	void	*mlx_player; //défini dans ft_associate_files_to_images
 	void	*mlx_items; //défini dans ft_associate_files_to_images
 	void	*mlx_empty_spaces; //défini dans ft_associate_files_to_images
+	void	ft_game_inputs(struct s_Map *map_ptr);
 	char	map_title[100]; //défini dans filler
 	int		size_x; //défini dans filler
 	int		count_mouv;
@@ -40,6 +45,16 @@ struct  s_Map
 	int		width;
 	int		heigth;
 };
+
+/* KEY CODES */
+
+# define ESC 65307
+# define GO_UP 119
+# define GO_DOWN 115
+# define GO_LEFT 97
+# define GO_RIGHT 100
+
+/* FUNCTIONS */
 
 int		ft_recup_map_size(char *file_name);
 char	**ft_recup_map(struct s_Map *map_ptr);
@@ -59,6 +74,6 @@ int		ft_check_file_name(char *file_name);
 int		ft_check_all_errors(struct s_Map *map_ptr);
 char	*ft_strcpy(char *dest, char *src);
 void	ft_init_game(struct s_Map *map_ptr);
-int 	key_hook(int keycode, struct s_Map *map_ptr);
+int 	handle_key(int keycode, struct s_Map *map_ptr);
 
 #endif
