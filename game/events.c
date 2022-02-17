8cf981eae9d0bf6ebe99877fcdad69e1326f8d4c
Camille BARBIT <cbarbit@e2r4p23.clusters.42paris.fr>
2022-02-17 12:50:58 +0100

    added function ft_game_inputs and delt with keycodes as defined in .h

--- game/events.c
+++ game/events.c
@@ -6,25 +6,30 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 16:19:59 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/17 11:57:33 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/17 12:46:09 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../so_long.h"
+#include "../so_long.h"
 
 /*
 Closing all images + Closing window 
 */
 
-void	ft_exit_game(struc s_Map *map_ptr)
+void	ft_exit_game(struct s_Map *map_ptr)
 {
     mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_walls);
 	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_items);
 	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_player);
 	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_exit);
 	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_empty_spaces);
-	mlx_destroy_window(map_ptr->mlx_ptr, map_ptr->window_ptr);
+	mlx_destroy_window(map_ptr->mlx_ptr, map_ptr->mlx_window);
 	ft_free_map(map_ptr);	
+}
+
+void	ft_exit_game_properly(struct s_Map *map_ptr)
+{
+	
 }
 
 int	handle_key(int keycode, struct s_Map *map_ptr)
@@ -48,7 +53,7 @@ Red cross + Keypress
 
 void	ft_game_inputs(struct s_Map *map_ptr)
 {
-	mlx_hook(game->mlx_win, 33, 1L << 5, ft_exit_game, map_ptr);
-	mlx_hook(game->mlx_win, 2, 1L << 0, handle_key, map_ptr);
+	mlx_hook(map_ptr->mlx_window, 33, 1L << 5, ft_exit_game, map_ptr);
+	mlx_hook(map_ptr->mlx_window, 2, 1L << 0, handle_key, map_ptr);
 	//mlx_hook(game->mlx_win, 9, 1L << 21, &init_resume_window, game);
 }	
