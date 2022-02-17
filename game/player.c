9dac5ebf031c2e0d1f150974b75e6ea4d3f97a20
Camille BARBIT <cbarbit@e2r4p23.clusters.42paris.fr>
2022-02-17 14:40:26 +0100

    new version to move player

--- game/player.c
+++ game/player.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 12:24:05 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/17 12:59:48 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/17 14:30:13 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,46 @@ Functions ensuring that player can move
 
 void    ft_move_up(struct s_Map *map_ptr)
 {
-    int x;
-    int y;
-
-    x = map_ptr->player_position_x;
-    y = map_ptr->player_position_y;
-    if(map_ptr->map_tab[y - 1][x] != '1')
-    {
-        y--;
-        get_items(map_ptr);
-        if (map_ptr->map_tab[y][x] == '0')
-        {
-            map_ptr->map_tab[y][x] = 'P';
-        }
-    }
+	if (map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] != '1')
+	{
+		if (map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] == 'C')
+		{
+			map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] == 'P';
+			map_ptr->count_items--;
+			ft_redraw_map();
+		}
+		else if (map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] == '0')
+		{
+			map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] == 'P';
+			ft_redraw_map();
+		}
+		else if (map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] == 'E' 
+		&& map_ptr->count_items == 0)
+		{
+			
+		}
+	}
 	else
 		printf("Error\nMoving forward is impossible!");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void    ft_move_left(struct s_Map *map_ptr)
 {
