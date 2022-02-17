418c8efd0f545f9b46bb2e3f7d0f591a768d4ca1
Camille BARBIT <cbarbit@e2r4p23.clusters.42paris.fr>
2022-02-17 11:59:05 +0100

    added functions to deal with input

--- game/events.c
+++ game/events.c
@@ -3,28 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   events.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 16:19:59 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/14 16:37:04 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/02/17 11:57:33 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../so_long.h"
 
-int handle_key(int keycode, struct s_Map *map_ptr) //This is the function called
+/*
+Closing all images + Closing window 
+*/
+
+void	ft_exit_game(struc s_Map *map_ptr)
 {
-    if (keycode == 119)
-        ft_move_up(map_ptr);
-    else if (keycode == 97)
-        ft_move_left(map_ptr);
-    else if (keycode == 115)
-        ft_move_down(map_ptr);
-    else if (keycode == 100)
-        ft_move_right(map_ptr);
-    else if (keycode == 65307) //esc button
-        return (1);
-    return (0);
+    mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_walls);
+	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_items);
+	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_player);
+	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_exit);
+	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_empty_spaces);
+	mlx_destroy_window(map_ptr->mlx_ptr, map_ptr->window_ptr);
+	ft_free_map(map_ptr);	
 }
 
-int handle_mouse()
+int	handle_key(int keycode, struct s_Map *map_ptr)
+{
+	if (keycode == GO_UP)
+		ft_move_up(map_ptr);
+	else if (keycode == GO_LEFT)
+		ft_move_left(map_ptr);
+	else if (keycode == GO_DOWN)
+		ft_move_down(map_ptr);
+	else if (keycode == GO_RIGHT)
+		ft_move_right(map_ptr);
+	else if (keycode == ESC)
+		ft_exit_game(map_ptr);
+	return (0);
+}
+
+/*
+Red cross + Keypress 
+*/
+
+void	ft_game_inputs(struct s_Map *map_ptr)
+{
+	mlx_hook(game->mlx_win, 33, 1L << 5, ft_exit_game, map_ptr);
+	mlx_hook(game->mlx_win, 2, 1L << 0, handle_key, map_ptr);
+	//mlx_hook(game->mlx_win, 9, 1L << 21, &init_resume_window, game);
+}	
