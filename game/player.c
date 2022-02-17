d3d58b36eb888ff06d7119f829e2e4df5921ed9b
Camille BARBIT <cbarbit@e2r4p23.clusters.42paris.fr>
2022-02-17 13:00:20 +0100

    starting to move the player

--- game/player.c
+++ game/player.c
@@ -3,27 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   player.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 12:24:05 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/14 16:20:32 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/02/17 12:59:48 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
 
-static void    ft_move_up(struct s_Map *map_ptr)
+/*
+Functions ensuring that player can move
+*/
+
+void    ft_move_up(struct s_Map *map_ptr)
 {
-    if(map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] != 1)
+    int x;
+    int y;
+
+    x = map_ptr->player_position_x;
+    y = map_ptr->player_position_y;
+    if(map_ptr->map_tab[y - 1][x] != '1')
     {
-        map_ptr->player_position_y--;
-		get_items(map_ptr);
+        y--;
+        get_items(map_ptr);
+        if (map_ptr->map_tab[y][x] == '0')
+        {
+            map_ptr->map_tab[y][x] = 'P';
+        }
     }
 	else
 		printf("Error\nMoving forward is impossible!");
 }
 
-static void    ft_move_left(struct s_Map *map_ptr)
+void    ft_move_left(struct s_Map *map_ptr)
 {
     if(map_ptr->map_tab[map_ptr->player_position_y][map_ptr->player_position_x - 1] !=1)
     {
@@ -34,7 +47,7 @@ static void    ft_move_left(struct s_Map *map_ptr)
 		printf("Error\nMoving left is impossible!");
 }
 
-static void    ft_move_down(struct s_Map *map_ptr)
+void    ft_move_down(struct s_Map *map_ptr)
 {
     if(map_ptr->map_tab[map_ptr->player_position_y + 1][map_ptr->player_position_x] !=1)
     {
@@ -45,7 +58,7 @@ static void    ft_move_down(struct s_Map *map_ptr)
 		printf("Error\nMoving down is impossible!");
 }
 
-static void    ft_move_right(struct s_Map *map_ptr)
+void    ft_move_right(struct s_Map *map_ptr)
 {
     if(map_ptr->map_tab[map_ptr->player_position_y][map_ptr->player_position_x + 1] !=1)
     {
