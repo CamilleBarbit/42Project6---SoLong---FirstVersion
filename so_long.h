59a74d02b989251f7894ef0c704d5cd0e665d3ac
Camille BARBIT <cbarbit@e2r4p23.clusters.42paris.fr>
2022-02-17 14:38:44 +0100

    added total_items

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:34:06 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/17 12:48:30 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/17 14:37:10 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,7 @@ struct  s_Map
 	int		size_x; //défini dans filler
 	int		count_mouv;
 	int		count_items; //défini dans check_map_char
+	int		total_items;
 	int		width;
 	int		heigth;
 };
