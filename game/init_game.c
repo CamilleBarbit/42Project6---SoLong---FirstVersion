b4517083b1b728b29d5422ffec5b91d7ed38086b
Camille BARBIT <cbarbit@e2r4p23.clusters.42paris.fr>
2022-02-17 10:41:03 +0100

    added conditions in case of errors

--- game/init_game.c
+++ game/init_game.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 11:47:07 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/16 15:52:23 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/17 10:30:02 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,20 @@
 void	ft_associate_files_to_images(struct s_Map *map_ptr)
 {
 	map_ptr->mlx_items = mlx_xpm_file_to_image(map_ptr->mlx_ptr, "images/items.xpm", &map_ptr->width, &map_ptr->heigth);
+	if (map_ptr->mlx_items == NULL)
+		printf("Error\nFile could not be converted into image!");
 	map_ptr->mlx_player = mlx_xpm_file_to_image(map_ptr->mlx_ptr, "images/player.xpm", &map_ptr->width, &map_ptr->heigth);
+	if (map_ptr->mlx_player == NULL)
+		printf("Error\nFile could not be converted into image!");
 	map_ptr->mlx_exit = mlx_xpm_file_to_image(map_ptr->mlx_ptr, "images/exit.xpm", &map_ptr->width, &map_ptr->heigth);
+	if (map_ptr->mlx_exit == NULL)
+		printf("Error\nFile could not be converted into image!");
 	map_ptr->mlx_empty_spaces = mlx_xpm_file_to_image(map_ptr->mlx_ptr, "images/empty_spaces.xpm", &map_ptr->width, &map_ptr->heigth);
+	if (map_ptr->mlx_empty_spaces == NULL)
+		printf("Error\nFile could not be converted into image!");
 	map_ptr->mlx_walls = mlx_xpm_file_to_image(map_ptr->mlx_ptr, "images/walls.xpm", &map_ptr->width, &map_ptr->heigth);
+	if (map_ptr->mlx_walls == NULL)
+		printf("Error\nFile could not be converted into image!");
 }
 
 void	ft_init_game(struct s_Map *map_ptr)
@@ -31,4 +41,5 @@ void	ft_init_game(struct s_Map *map_ptr)
 		printf("Error\nFailed to create a new window!");
 	ft_associate_files_to_images(map_ptr);
 	ft_draw_map(map_ptr);
+	//mlx_key_hook(map_ptr->mlx_window, handle_key, map_ptr);
 }
