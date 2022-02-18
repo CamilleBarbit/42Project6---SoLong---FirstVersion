5b093d5b029f14d42885fcdbad1ba4d74b6606dc
Camille Barbit <barbitcamille@gmail.com>
2022-02-18 12:10:50 +0100

    Updated events with ft_exit_game / All clear

--- game/events.c
+++ game/events.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   events.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 16:19:59 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/17 13:48:54 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/18 12:07:13 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,19 @@
 Closing all images + Closing window 
 */
 
-void	ft_exit_game(struct s_Map *map_ptr)
+int	ft_exit_game(struct s_Map *map_ptr)
 {
-    mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_walls);
-	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_items);
-	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_player);
-	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_exit);
-	mlx_destroy_image(map_ptr->mlx_ptr, map_ptr->mlx_empty_spaces);
-	mlx_destroy_window(map_ptr->mlx_ptr, map_ptr->mlx_window);
-	ft_free_map(map_ptr);	
+	ft_free_game(map_ptr);
+	ft_free_map(map_ptr);
+	printf("Thank you for playing! Come back soon!");
+	exit (1);	
 }
 
-void	ft_exit_game_properly(struct s_Map *map_ptr)
-{
-	
-}
+/*
+Dealing with player moves
+*/
 
-int	handle_key(int keycode, struct s_Map *map_ptr)
+int	ft_handle_key(int keycode, struct s_Map *map_ptr)
 {
 	if (keycode == GO_UP)
 		ft_move_up(map_ptr);
@@ -53,7 +49,7 @@ Red cross + Keypress
 
 void	ft_game_inputs(struct s_Map *map_ptr)
 {
-	mlx_hook(map_ptr->mlx_window, 33, 1L << 5, ft_exit_game, map_ptr);
-	mlx_hook(map_ptr->mlx_window, 2, 1L << 0, handle_key, map_ptr);
+	mlx_hook(map_ptr->mlx_window, 33, 1L << 5, &ft_exit_game, map_ptr);
+	mlx_hook(map_ptr->mlx_window, 2, 1L << 0, &ft_handle_key, map_ptr);
 	//mlx_hook(game->mlx_win, 9, 1L << 21, &init_resume_window, game);
 }	
