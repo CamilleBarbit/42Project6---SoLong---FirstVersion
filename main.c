151e95775682936352d831589ef16391264f87a8
Camille Barbit <barbitcamille@gmail.com>
2022-02-18 12:14:23 +0100

    Modified ft_filler_basics - initialized all my variables

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:34:16 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/17 12:14:44 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/18 11:29:31 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,23 @@ Filling my structure with basic variables
 
 void	ft_filler_basics(s_Map *map_ptr, char *str)
 {
-	map_ptr->file_name = ft_sdup(str); //nom de mon ficher .ber
-	map_ptr->map_size = ft_recup_map_size(map_ptr->file_name); //taille de ma map (y)
-	map_ptr->map_tab = ft_recup_map(map_ptr); //Je récupère la map dans un char **
-	map_ptr->size_x = ft_len(map_ptr->map_tab[0]) - 1; //La taille de ma première ligne - le '\n'
-	ft_strcpy(map_ptr->map_title, "so_long"); //Le nom de ma fenêtre de jeu
-	map_ptr->width = 32;
-	map_ptr->heigth = 32;
+	map_ptr->count_mouv = 0;
+	map_ptr->total_items = 0;
+	map_ptr->remaining_items = 0;
+	map_ptr->player_position_x = 0;
+	map_ptr->player_position_y = 0;
+	map_ptr->file_name = ft_sdup(str);
+	map_ptr->map_size = ft_recup_map_size(map_ptr->file_name);
+	map_ptr->map_tab = ft_recup_map(map_ptr);
+	map_ptr->size_x = ft_len(map_ptr->map_tab[0]) - 1;
+	ft_strcpy(map_ptr->map_title, "so_long");
+	map_ptr->mlx_ptr = NULL;
+	map_ptr->mlx_window = NULL;
+	map_ptr->mlx_exit = NULL;
+	map_ptr->mlx_walls = NULL;
+	map_ptr->mlx_player = NULL;
+	map_ptr->mlx_items = NULL;
+	map_ptr->mlx_empty_spaces = NULL;
 }
 
 int	main(int argc, char **argv)
@@ -35,16 +45,17 @@ int	main(int argc, char **argv)
 	{
 		map_ptr = malloc(sizeof(struct s_Map));
 		if (!map_ptr)
-			return (1);
+			return (printf("Error\nFailed to allocate memory\n!"), 1);
 		if (ft_check_file_name(argv[1]) == 1)
-			return (free(map_ptr), 1);
+			return (ft_free_map(map_ptr), 1);
 		ft_filler_basics(map_ptr, argv[1]);
 		if (ft_check_all_errors(map_ptr) == 1)
-			return (free(map_ptr), 1);
+			return (ft_free_map(map_ptr), 1);
 		ft_init_game(map_ptr);
 		ft_game_inputs(map_ptr);
 		mlx_loop(map_ptr->mlx_ptr);
 	}
 	else
-		printf("Error\nMissing arguments!\n");
+		printf("Error\nMissing a <map>.ber as second argument!\n");
+	return (0);
 }
