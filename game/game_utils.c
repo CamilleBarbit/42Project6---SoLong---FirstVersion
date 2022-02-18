0c004fdf8aa43c4081552e1982807f60c642eeb2
Camille Barbit <barbitcamille@gmail.com>
2022-02-18 12:12:54 +0100

    Modified get_number_of_items from int to void!

--- game/game_utils.c
+++ game/game_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   game_utils.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 11:46:15 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/17 14:40:49 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/18 11:59:05 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,27 +43,24 @@ void	get_player_position(struct s_Map *map_ptr)
 Getting number of items
 */
 
-int	get_number_of_items(struct s_Map *map_ptr)
+void	get_number_of_items(struct s_Map *map_ptr)
 {
 	int	x;
 	int	y;
-	int	count;
 
 	x = 0;
 	y = 0;
-	count = 0;
 	while (map_ptr->map_tab[y])
 	{
 		x = 0;
 		while (map_ptr->map_tab[y][x])
 		{
 			if (map_ptr->map_tab[y][x] == 'C')
-				count += 1;
+				map_ptr->total_items += 1;
 			x++;
 		}
 		y++;
 	}
-	return (count);
 }
 
 void	ft_redraw_map(struct s_Map *map_ptr, )
