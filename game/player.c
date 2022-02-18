0d08030c7167ab13e4fd7ca6f592829be2fe0fbf
Camille Barbit <barbitcamille@gmail.com>
2022-02-18 19:01:34 +0100

    Updated version / trying another function to move the player correctly

--- game/player.c
+++ game/player.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   player.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 12:24:05 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/17 14:30:13 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/18 18:59:55 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,46 +18,30 @@ Functions ensuring that player can move
 
 void    ft_move_up(struct s_Map *map_ptr)
 {
-	if (map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] != '1')
+	if (map_ptr->map_tab[map_ptr->player_y - 1][map_ptr->player_x] != '1')
 	{
-		if (map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] == 'C')
+		if ((map_ptr->map_tab[map_ptr->player_y - 1][map_ptr->player_x] == 'E') 
+		&& (map_ptr->items_collected == map_ptr->total_items))
 		{
-			map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] == 'P';
-			map_ptr->count_items--;
-			ft_redraw_map();
+			ft_redraw_map(map_ptr);
 		}
-		else if (map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] == '0')
+		else if ((map_ptr->map_tab[map_ptr->player_y - 1][map_ptr->player_x] == 'C')
+		|| (map_ptr->map_tab[map_ptr->player_y - 1][map_ptr->player_x] == '0'))
 		{
-			map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] == 'P';
-			ft_redraw_map();
-		}
-		else if (map_ptr->map_tab[map_ptr->player_position_y - 1][map_ptr->player_position_x] == 'E' 
-		&& map_ptr->count_items == 0)
-		{
-			
-		}
+			map_ptr->map_tab[map_ptr->player_y][map_ptr->player_x] == '0';
+			map_ptr->player_position_y--;
+			ft_get_items(map_ptr);
+			ft_redraw_map(map_ptr);
+		}	
+		else
+			printf("Error\nYou are not done yet!\n");
+			break;
 	}
 	else
 		printf("Error\nMoving forward is impossible!");
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
 
 void    ft_move_left(struct s_Map *map_ptr)
 {
@@ -91,3 +75,5 @@ void    ft_move_right(struct s_Map *map_ptr)
 	else
 		printf("Error\nMoving right is impossible!");
 }
+
+*/
