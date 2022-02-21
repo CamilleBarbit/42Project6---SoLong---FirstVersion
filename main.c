3440d0f2f1d0ae20d96b281a05b8a3d94f772ca7
Camille BARBIT <cbarbit@e2r12p2.clusters.42paris.fr>
2022-02-21 15:31:22 +0100

    Game playing but character not moving

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:34:16 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/18 18:49:15 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/02/21 15:30:50 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ void	ft_filler_basics(s_Map *map_ptr, char *str)
 	map_ptr->items_collected = 0;
 	map_ptr->player_x = 0;
 	map_ptr->player_y = 0;
+	map_ptr->im_heigth = 80;
+	map_ptr->im_width = 80;
 	map_ptr->file_name = ft_sdup(str);
 	map_ptr->map_size = ft_recup_map_size(map_ptr->file_name);
 	map_ptr->map_tab = ft_recup_map(map_ptr);
@@ -37,6 +39,7 @@ void	ft_filler_basics(s_Map *map_ptr, char *str)
 	map_ptr->mlx_empty_spaces = NULL;
 }
 
+
 int	main(int argc, char **argv)
 {
 	s_Map	*map_ptr;
@@ -47,13 +50,14 @@ int	main(int argc, char **argv)
 		if (!map_ptr)
 			return (printf("Error\nFailed to allocate memory\n!"), 1);
 		if (ft_check_file_name(argv[1]) == 1)
-			return (ft_free_map(map_ptr), 1);
+			return (1);//ft_free_map(map_ptr), 1);
 		ft_filler_basics(map_ptr, argv[1]);
 		if (ft_check_all_errors(map_ptr) == 1)
-			return (ft_free_map(map_ptr), 1);
+			return (1);//ft_free_map(map_ptr), 1);
 		ft_init_game(map_ptr);
 		ft_game_inputs(map_ptr);
 		mlx_loop(map_ptr->mlx_ptr);
+		ft_dealloc(map_ptr);
 	}
 	else
 		printf("Error\nMissing a <map>.ber as second argument!\n");
