60b2f8ae43703be8bdb052486cee031a11cb37e5
Camille BARBIT <cbarbit@e2r12p2.clusters.42paris.fr>
2022-02-21 14:35:07 +0100

    Updated version - removed defined variables

--- game/print_map.c
+++ game/print_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 11:32:48 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/18 11:46:31 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/02/21 14:27:15 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,15 @@ void	ft_draw_map(struct s_Map *map_ptr)
 		while (map_ptr->map_tab[y][x])
 		{
 			if (map_ptr->map_tab[y][x] == '1')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_walls, WIDTH * x, HEIGTH * y);
+				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_walls, map_ptr->im_width * x, map_ptr->im_heigth * y);
 			else if (map_ptr->map_tab[y][x] == 'C')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_items, WIDTH * x, HEIGTH * y);
+				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_items, map_ptr->im_width * x, map_ptr->im_heigth * y);
 			else if (map_ptr->map_tab[y][x] == 'P')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_player, WIDTH * x, HEIGTH * y);
+				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_player, map_ptr->im_width * x, map_ptr->im_heigth * y);
 			else if (map_ptr->map_tab[y][x] == 'E')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_exit, WIDTH * x, HEIGTH * y);
+				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_exit, map_ptr->im_width * x, map_ptr->im_heigth * y);
 			else if (map_ptr->map_tab[y][x] == '0')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_empty_spaces, WIDTH * x, HEIGTH * y);
+				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_empty_spaces, map_ptr->im_width * x, map_ptr->im_heigth * y);
 			x++;
 		}
 		y++;
