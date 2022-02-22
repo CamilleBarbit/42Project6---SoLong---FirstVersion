4048465b1a8a85ddf2d018ee8bca9374b4daa7a6
Camille BARBIT <cbarbit@e2r10p7.clusters.42paris.fr>
2022-02-22 15:05:13 +0100

    Newest version - All errors taken into account

--- errors/checkerror_file.c
+++ errors/checkerror_file.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 15:44:11 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/02/21 16:58:06 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/22 15:00:10 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,17 @@ int	ft_check_file_name(char *file_name)
 	char	*str;
     int		count;
 
-	i = 0;
+	i = 1;
 	str = ".ber";
     count = 0;
+	if (ft_len(file_name) < 4)
+		return (printf("Error\nProblem with file name!\n"), 1);
 	while (file_name[i])
 	{
-		if (file_name[i] == '.' && count == 0)
+		if (file_name[i] == '.' && count == 0 && file_name[0])
 		{
             count = 1;
-			if (ft_strcmp(file_name + i, str) == 0  && file_name[i + 4] == '\0' && ft_len(file_name) > 4)
+			if (ft_strcmp(file_name + i, str) == 0  && file_name[i + 4] == '\0' && file_name[i - 1] != '/')
 				return (0);
 		}
 		i++;
@@ -38,5 +40,21 @@ int	ft_check_file_name(char *file_name)
 	return(printf("Error\nProblem with file name!\n"), 1);
 }
 
+int	ft_try_to_open_file(char *file_name)
+{
+	int	fd;
 
-//What happens if it is 'map/.ber'??
+	fd = open(file_name, O_RDONLY);
+	if (fd < 0)
+		return (printf("Error\nCould not open file!"), 1);
+	return (0);
+}
+
+int ft_check_all_errors_file_name(char *file_name)
+{
+	if (ft_check_file_name(file_name) == 1)
+		return (1);
+	if (ft_try_to_open_file(file_name) == 1)
+		return (1);
+	return (0);
+}
