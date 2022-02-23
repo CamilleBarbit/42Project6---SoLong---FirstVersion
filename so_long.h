4534b7b0c2b47d94db7dbf97b46b217f7d6ee0ea
Camille BARBIT <cbarbit@e2r10p12.clusters.42paris.fr>
2022-02-23 17:20:15 +0100

    NORMINETTE OK

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:34:06 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/23 15:23:22 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/23 17:19:55 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 
 /* STRUCTURE */
 
-struct	s_map
+typedef struct s_Map
 {
 	char	**map_tab;
 	char	*file_name;
@@ -44,7 +44,7 @@ struct	s_map
 	int		im_width;
 	int		im_heigth;
 	int		temp;
-};
+}	t_map;
 
 /* KEY CODES */
 
@@ -65,35 +65,35 @@ struct	s_map
 /* FUNCTIONS */
 
 int		ft_recup_map_size(char *file_name);
-char	**ft_recup_map(struct s_Map *map_ptr);
-int		ft_check_map_form(struct s_Map *map_ptr);
-int		ft_check_walls(struct s_Map *map_ptr);
-int		ft_check_map_char(struct s_Map *map_ptr);
-void	ft_free_map(struct s_Map *map_ptr);
-int		ft_filler_basics(s_Map *map_ptr, char *str);
-void	ft_associate_files_to_images(struct s_Map *map_ptr);
-void	get_player_position(struct s_Map *map_ptr);
-void	get_number_of_items(struct s_Map *map_ptr);
-void	ft_draw_map(struct s_Map *map_ptr);
+char	**ft_recup_map(struct t_map *map_ptr);
+int		ft_check_map_form(struct t_map *map_ptr);
+int		ft_check_walls(struct t_map *map_ptr);
+int		ft_check_map_char(struct t_map *map_ptr);
+void	ft_free_map(struct t_map *map_ptr);
+int		ft_filler_basics(struct t_map *map_ptr, char *str);
+void	ft_associate_files_to_images(struct t_map *map_ptr);
+void	get_player_position(struct t_map *map_ptr);
+void	get_number_of_items(struct t_map *map_ptr);
+void	ft_draw_map(struct t_map *map_ptr);
 char	*ft_sdup(char *src);
 int		ft_len(char *str);
 int		ft_strcmp(char *s1, char *s2);
 int		ft_check_file_name(char *file_name, int j, char *str, int count);
-int		ft_check_all_errors(struct s_Map *map_ptr);
+int		ft_check_all_errors(struct t_map *map_ptr);
 char	*ft_strcpy(char *dest, char *src);
-void	ft_init_game(struct s_Map *map_ptr);
-void	ft_game_inputs(struct s_Map *map_ptr);
-int		handle_key(int keycode, struct s_Map *map_ptr);
-void	ft_move_right(struct s_Map *map_ptr);
-void	ft_move_left(struct s_Map *map_ptr);
-void	ft_move_up(struct s_Map *map_ptr);
-void	ft_move_down(struct s_Map *map_ptr);
-void	ft_dealloc(struct s_Map *map_ptr);
-void	ft_free_game(struct s_Map *map_ptr);
-int		ft_exit_game(struct s_Map *map_ptr);
+void	ft_init_game(struct t_map *map_ptr);
+void	ft_game_inputs(struct t_map *map_ptr);
+int		handle_key(int keycode, struct t_map *map_ptr);
+void	ft_move_right(struct t_map *map_ptr);
+void	ft_move_left(struct t_map *map_ptr);
+void	ft_move_up(struct t_map *map_ptr);
+void	ft_move_down(struct t_map *map_ptr);
+void	ft_dealloc(struct t_map *map_ptr);
+void	ft_free_game(struct t_map *map_ptr);
+int		ft_exit_game(struct t_map *map_ptr);
 int		ft_try_to_open_file(char *file_name);
 int		ft_check_all_errors_file_name(char *file_name);
-void	ft_replace_letters(struct s_Map *map_ptr);
-void	ft_making_the_move(struct s_Map *map_ptr);
+void	ft_replace_letters(struct t_map *map_ptr);
+void	ft_making_the_move(struct t_map *map_ptr);
 
 #endif
