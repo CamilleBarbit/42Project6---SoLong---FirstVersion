88e074ee6a174a2b0c939cd4abf1a706adc3b6bf
Camille BARBIT <cbarbit@e2r10p12.clusters.42paris.fr>
2022-02-23 15:18:11 +0100

    All clear - added new functions!

--- game/game_utils.c
+++ game/game_utils.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 11:46:15 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/21 17:17:54 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/23 15:15:43 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,4 +61,31 @@ void	get_number_of_items(struct s_Map *map_ptr)
 		}
 		y++;
 	}
+}
+
+
+/*
+Function to ligthen the player's moves
+*/
+
+void	ft_replace_letters(struct s_Map *map_ptr)
+{
+    if (map_ptr->temp == 0)
+		map_ptr->map_tab[map_ptr->player_y][map_ptr->player_x] = '0';
+	if (map_ptr->temp == 1)
+	{
+		map_ptr->map_tab[map_ptr->player_y][map_ptr->player_x] = 'E';
+		map_ptr->temp = 0;
+	}
+}
+
+/*
+Function t lighten the player's moves
+*/
+
+void	ft_making_the_move(struct s_Map *map_ptr)
+{
+	map_ptr->count_moves++;
+	printf("You have made: %d moves!\n", map_ptr->count_moves);
+	ft_draw_map(map_ptr);
 }
