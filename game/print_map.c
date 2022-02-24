76c00756a27f79b6b212e1d4197792e08ed4a1ae
Camille BARBIT <cbarbit@e2r3p16.clusters.42paris.fr>
2022-02-24 14:29:17 +0100

    NORMINETTE OK

--- game/print_map.c
+++ game/print_map.c
@@ -6,11 +6,17 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/12 11:32:48 by camillebarb       #+#    #+#             */
-/*   Updated: 2022/02/23 18:00:08 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/24 14:28:39 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
+
+void	ft_function_to_draw(t_map *map_ptr, void *ptr_file, int x, int y)
+{
+	mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window,
+		ptr_file, map_ptr->im_width * x, map_ptr->im_heigth * y);
+}
 
 void	ft_draw_map(t_map *map_ptr)
 {
@@ -24,15 +30,15 @@ void	ft_draw_map(t_map *map_ptr)
 		while (map_ptr->map_tab[y][x])
 		{
 			if (map_ptr->map_tab[y][x] == '1')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_walls, map_ptr->im_width * x, map_ptr->im_heigth * y);
+				ft_function_to_draw(map_ptr, map_ptr->mlx_walls, x, y);
 			else if (map_ptr->map_tab[y][x] == 'C')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_items, map_ptr->im_width * x, map_ptr->im_heigth * y);
+				ft_function_to_draw(map_ptr, map_ptr->mlx_items, x, y);
 			else if (map_ptr->map_tab[y][x] == 'P')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_player, map_ptr->im_width * x, map_ptr->im_heigth * y);
+				ft_function_to_draw(map_ptr, map_ptr->mlx_player, x, y);
 			else if (map_ptr->map_tab[y][x] == 'E')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_exit, map_ptr->im_width * x, map_ptr->im_heigth * y);
+				ft_function_to_draw(map_ptr, map_ptr->mlx_exit, x, y);
 			else if (map_ptr->map_tab[y][x] == '0')
-				mlx_put_image_to_window(map_ptr->mlx_ptr, map_ptr->mlx_window, map_ptr->mlx_empty_spaces, map_ptr->im_width * x, map_ptr->im_heigth * y);
+				ft_function_to_draw(map_ptr, map_ptr->mlx_empty_spaces, x, y);
 			x++;
 		}
 		y++;
