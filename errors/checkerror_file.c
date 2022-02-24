651f6627848880e6ec11b7e10a37d9c3ba1bc1fd
Camille BARBIT <cbarbit@e2r3p16.clusters.42paris.fr>
2022-02-24 14:42:35 +0100

    Added a function to verify if it is possible to open xmp files

--- errors/checkerror_file.c
+++ errors/checkerror_file.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 15:44:11 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/02/22 17:21:24 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/24 14:40:14 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,11 +54,36 @@ int	ft_try_to_open_file(char *file_name)
 	return (0);
 }
 
+int	ft_try_to_open_xpm(void)
+{
+	int	fd_1;
+	int	fd_2;
+	int	fd_3;
+	int	fd_4;
+	int	fd_5;
+
+	fd_1 = open(ITEMS_IM, O_RDONLY);
+	fd_2 = open(PLAYER_IM, O_RDONLY);
+	fd_3 = open(EXIT_IM, O_RDONLY);
+	fd_4 = open(EMPTY_IM, O_RDONLY);
+	fd_5 = open(WALLS_IM, O_RDONLY);
+	if (fd_1 < 0 || fd_2 < 0 || fd_3 < 0 || fd_4 < 0 || fd_5 < 0)
+		return (printf("Error\nCould not open <xpm> file!\n"), 1);
+	close (fd_1);
+	close (fd_2);
+	close (fd_3);
+	close (fd_4);
+	close (fd_5);
+	return (0);
+}
+
 int	ft_check_all_errors_file_name(char *file_name)
 {
 	if (ft_check_file_name(file_name, ft_len(file_name), ".ber", 0) == 1)
 		return (1);
 	if (ft_try_to_open_file(file_name) == 1)
+		return (1);
+	if (ft_try_to_open_xpm() == 1)
 		return (1);
 	return (0);
 }
