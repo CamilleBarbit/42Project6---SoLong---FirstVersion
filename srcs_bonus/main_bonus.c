3a85a49362890126403b023ca4ac28bdaad93258
Camille BARBIT <cbarbit@e2r5p17.clusters.42paris.fr>
2022-02-25 14:36:47 +0100

    BONUS ADDED

---
+++ srcs_bonus/main_bonus.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/25 14:35:06 by cbarbit           #+#    #+#             */
+/*   Updated: 2022/02/25 14:35:42 by cbarbit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+/* 
+Filling my structure with basic variables
+*/
+
+int	ft_filler_basics(t_map *map_ptr, char *str)
+{
+	map_ptr->count_moves = 0;
+	map_ptr->total_items = 0;
+	map_ptr->collected_items = 0;
+	map_ptr->player_x = 0;
+	map_ptr->player_y = 0;
+	map_ptr->temp = 0;
+	map_ptr->im_heigth = 80;
+	map_ptr->im_width = 80;
+	map_ptr->file_name = ft_sdup(str);
+	map_ptr->map_size = ft_recup_map_size(map_ptr->file_name);
+	if (map_ptr->map_size == -1)
+		return (printf("Error\nEmpty file!\n"), 1);
+	map_ptr->map_tab = ft_recup_map(map_ptr);
+	map_ptr->size_x = ft_len(map_ptr->map_tab[0]) - 1;
+	ft_strcpy(map_ptr->map_title, "so_long");
+	map_ptr->mlx_ptr = NULL;
+	map_ptr->mlx_window = NULL;
+	map_ptr->mlx_exit = NULL;
+	map_ptr->mlx_walls = NULL;
+	map_ptr->mlx_player = NULL;
+	map_ptr->mlx_items = NULL;
+	map_ptr->mlx_empty_spaces = NULL;
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_map	*map_ptr;
+
+	if (argc == 2)
+	{
+		map_ptr = malloc(sizeof(t_map));
+		if (!map_ptr)
+			return (printf("Error\nFailed to allocate memory\n!"), 1);
+		if (ft_check_all_errors_file_name(argv[1]) == 1)
+			return (free(map_ptr), 1);
+		if (ft_filler_basics(map_ptr, argv[1]) == 1)
+			return (free(map_ptr->file_name), free(map_ptr), 1);
+		if (ft_check_all_errors(map_ptr) == 1)
+			return (ft_dealloc(map_ptr), 1);
+		ft_init_game(map_ptr);
+		ft_game_inputs(map_ptr);
+		mlx_loop(map_ptr->mlx_ptr);
+		ft_dealloc(map_ptr);
+	}
+	else
+		printf("Error\nMissing a <map>.ber as second argument!\n");
+	return (0);
+}
---
+++ srcs_bonus/utils_bonus.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/08 14:02:19 by cbarbit           #+#    #+#             */
+/*   Updated: 2022/02/25 14:36:11 by cbarbit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long_bonus.h"
+
+int	ft_len(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+char	*ft_sdup(char *src)
+{
+	char	*dest;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = ft_len(src);
+	dest = malloc(sizeof(char) * j + 1);
+	if (!dest)
+		return (0);
+	while (i < j)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
